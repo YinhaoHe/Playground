39da50756527343e9fdf05c9819f1191e28c53fb
YinhaoHe <yinhaoh@uci.edu>
2020-04-16 15:05:38 -0700

    Add interview questions folder

---
+++ InterviewQuestions/Maximum _Occurring_Character.java
@@ -0,0 +1,13 @@
+/*
+
+Maximum occurring character
+
+Return maximum occurring character in an input string.
+Write an efficient function to return maximum occurring character in the input string 
+e.g., if input string is “test” then function should return ‘t’.
+
+*/
+
+class solution {
+	
+}
