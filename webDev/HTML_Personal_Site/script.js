6cf12b4f1cdeb1307e8a7d7ea1000a18c6479686
YinhaoHe <yinhaoh@uci.edu>
2020-04-28 16:06:32 -0700

    Update personal website script

--- webDev/HTML_Personal_Site/script.js
+++ webDev/HTML_Personal_Site/script.js
@@ -2,3 +2,8 @@ function ButtonClick() {
   let userInput = prompt('Email me');
   console.log(userInput);
 }
+
+function readEmail() {
+  name = document.getElementById('name').value;
+  console.log(name);
+}
