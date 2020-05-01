8ac5e90b0a912c7ec3c10081016e30c0818ceddd
YinhaoHe <yinhaoh@uci.edu>
2020-05-01 12:26:55 -0700

    Add ns-3 Dev

---
+++ ns-3/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3-dev-scratch-simulator-debug', 'build/scratch/subdir/ns3-dev-subdir-debug', 'build/scratch/ns3-dev-myfirst-debug', 'build/scratch/ns3-dev-myfifth-debug', 'build/scratch/ns3-dev-mythird-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
