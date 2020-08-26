d4159d12f812c2587149bcaf08cd4da4230faf93
YinhaoHe <yinhaoh@uci.edu>
2020-08-25 23:54:53 -0700

    organize playground

--- webDev/HTML_Personal_Site/script.js
+++
@@ -1,9 +0,0 @@
-function ButtonClick() {
-  let userInput = prompt('Email me');
-  console.log(userInput);
-}
-
-function readEmail() {
-  name = document.getElementById('name').value;
-  console.log(name);
-}
